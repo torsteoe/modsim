c7d2c9f064997d1eb6c641b886a2543ada35c3a7
Torstein Ørbeck Eliassen <torsteoe@ntnu.no>
2020-03-10 14:49:00 +0100

    finish exercise

---
+++ oving8/oppg2.m
@@ -0,0 +1,19 @@
+%% 2)
+
+close all;
+clear;
+clc;
+
+lambda = -2;
+x0 = 1;
+f = @(t, x) lambda*x;
+syms x t;
+jac(x) = jacobian(f(t, x), x);
+
+T = 0:0.02:2;
+X = ImplicitEulerTemplate(f, jac, T, x0);
+
+plot(T, X);
+hold on ;
+plot(T, exp(lambda*T));
+legend('IRK solution', 'Real solution');
